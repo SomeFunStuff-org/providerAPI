c0317314357bf193930973b61e8ce3cd76dc9778
Wes Hediger <wes.hediger@gmail.com>
2018-05-03 22:58:14 -0500

    add getProvider and addProvider

--- javascript/providersDb.js
+++ javascript/providersDb.js
@@ -3,5 +3,12 @@ var knex = require('../config/knex');
 module.exports = {
     getProviders: function(){
        return knex('providers');
+    },
+    addProvider: function(data) {
+        return knex('providers').insert(data, 'ProviderID');
+    },
+    getProvider: function(id) {
+        return knex('providers')
+            .where('providersID', id);
     }
 }
--- routes/providers.js
+++ routes/providers.js
@@ -5,10 +5,25 @@ var router = express.Router();
 
 /* GET providers listing. */
 router.get('/getProviders', function(req, res, next) {
-    var providerList = db.getProviders()
-        .then(function(data){
-            res.send(data);
-        });
+    db.getProviders()
+    .then(function(data){
+        res.send(data);
+    });
+});
+
+router.get('/getProvider', function(req, res, next) {
+    db.getProvider(id)
+    .then(function(provider) {
+        res.send(provider);
+    });
+});
+
+//TODO: can users create Providers?
+router.post('/addProvider', function(req, res, next) {
+    db.addProvider(req.body)
+    .then(function(data){
+        res.send('status: 200 Successful');
+    });
 });
 
 module.exports = router;
